99f20c80732b2762eb027bb312d859b673a2b057
alicapshipshipship <imad@kitthq.com>
2024-10-27 12:54:48 +0400

    fc

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeScore = 0;
+let awayScore = 0;
+document.getElementById('away-score').innerText = awayScore;
+
+function addHomeScore1() {
+    homeScore += 1; 
+    document.getElementById('home-score').innerText = homeScore;
+    console.log(homeScore);
+}
+
+function addHomeScore2() {
+    homeScore += 2; 
+    document.getElementById('home-score').innerText = homeScore;
+    console.log(homeScore);
+}
+
+function addHomeScore3() {
+    homeScore += 3; 
+    document.getElementById('home-score').innerText = homeScore;
+    console.log(homeScore);
+}
+
+function addAwayScore1() {
+    awayScore += 1; 
+    document.getElementById('away-score').innerText = awayScore;
+    console.log(homeScore);
+}
+
+function addAwayScore2() {
+    awayScore += 2; 
+    document.getElementById('away-score').innerText = awayScore;
+    console.log(homeScore);
+}
+
+function addAwayScore3() {
+    awayScore += 3; 
+    document.getElementById('away-score').innerText = awayScore;
+    console.log(homeScore);
+}
+
+
+
